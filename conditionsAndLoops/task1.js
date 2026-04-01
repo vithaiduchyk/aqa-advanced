bbecb6bf6f15ea5bb986dd50c2fddb141960f859
Vitalii Haiduchyk <vit.haiduchyk@gmail.com>
2026-03-31 23:54:14 -0500

    task1 done

---
+++ conditionsAndLoops/task1.js
@@ -0,0 +1,16 @@
+let averageGrade = 91;
+
+if (averageGrade < 60) {
+    console.log('Unsatisfactorily');
+}else if (averageGrade <= 70){
+    console.log('Satisfactorily');
+}else if (averageGrade <= 80){
+    console.log('Good') 
+}else if (averageGrade <= 90){
+    console.log('Very good')
+}else if (averageGrade <= 100){
+    console.log('Perfect')
+}
+
+
+
