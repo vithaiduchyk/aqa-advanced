ddc596d51251140c2f770b0b6f09b2164560c3f1
Vitalii Haiduchyk <vit.haiduchyk@gmail.com>
2026-04-04 16:34:17 -0500

    added solution for task 1 in 3 ways

---
+++ introToFunction/task1.js
@@ -0,0 +1,13 @@
+// function calculateArea(width, height){
+//     return console.log(width * height);
+
+// };
+
+// const calculateArea = function(width, height) {
+//     return console.log(width * height);
+
+// };
+
+const calculateArea = (width, height) => {return console.log(width * height) };
+
+calculateArea(5, 10);
